553ab39889c111047c358606448b2d614f8ff52c
Timóteo Stifft <timoteostifft@gmail.com>
2022-09-16 11:20:02 -0300

    style: create page logo box

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import './styles/main.css'
+
+import logoImg from './assets/nlw-esports-logo.svg'
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="" />
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
+      </h1>
+    </div>
   )
 }
 
