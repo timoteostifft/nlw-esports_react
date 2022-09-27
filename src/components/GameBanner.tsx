5caf6118da5f7beb3d0a40ac362dbfc4b189da79
Timóteo Stifft <timoteostifft@gmail.com>
2022-09-27 11:40:48 -0300

    refactor: update gammer banner to a component

---
+++ src/components/GameBanner.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface GameBannerProps {
+  bannerUrl: string;
+  title: string;
+  adsCount: number;
+}
+
+const GameBanner: React.FC<GameBannerProps> = ({ bannerUrl, title, adsCount }) => {
+  return (
+    <a href="" className='relative rounded-lg overflow-hidden'>
+      <img src={bannerUrl} alt="" />
+      <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+        <strong className='font-bold text-white block'>{title}</strong>
+        <span className='text-zinc-300 text-sm'>{adsCount} anúncio(s)</span>
+      </div>
+    </a>
+  );
+}
+
+export default GameBanner;
